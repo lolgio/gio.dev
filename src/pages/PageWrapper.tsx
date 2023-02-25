dbdb88d1410eb15391d21939c9ed988282e73b4c
lolgio <gioguccimane@gmail.com>
2023-02-25 18:08:41 +1300

    mobile scroll

--- src/pages/PageWrapper.tsx
+++ src/pages/PageWrapper.tsx
@@ -11,7 +11,7 @@ const PageWrapper: Component<{
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
-            class="min-h-[calc(100vh-8rem)] overflow-hidden"
+            class="min-h-[calc(100vh-8rem)]"
         >
             {props.children}
         </Motion.div>
